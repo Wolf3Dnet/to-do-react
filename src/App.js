1edfa040192f9b0298fb744a60a03abcf5ff69b1
Wolf3Dnet <nikwolf1997@mail.ru>
2021-07-28 14:21:17 +0300

    rename

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import s from "./App.module.css";
 import Elements from "./components/elements/elements";
-import TodoForm from "./components/input/Todoform";
+import TodoForm from "./components/todoForm/Todoform";
 
 function App() {
   return (
