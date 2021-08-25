c2579a1d7c519f2d45fd55121e3e152c674da1e4
Wolf3Dnet <nikwolf1997@mail.ru>
2021-08-25 16:46:38 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,82 +1,22 @@
 import { observer } from 'mobx-react-lite';
-import React, { useCallback, useState } from 'react';
+import React, { useCallback } from 'react';
 import s from './App.module.css';
 import { Elements } from './components/elements/elements';
 import { TodoForm } from './components/todoForm/Todoform';
 import { todoList } from './store/TodoListStore';
 
 export const App = observer(() => {
-  const [list, setList] = useState([]);
-
-  const handleAddTodo = useCallback(
-    (title) => {
-      if (title !== '') {
-        todoList.addTodo({
-          id: Math.random(),
-          title: title,
-          isComplete: false,
-          isEdit: false,
-        });
-
-        console.log('handleAddTodo сработала');
-      }
-    },
-    [todoList]
-  );
-
-  const handleRemoveTodo = useCallback(
-    (id) => {
-      todoList.removeTodo(id);
-      console.log('handleRemoveTodo сработала');
-    },
-    [list]
-  );
-
-  const handleCompleteTodo = useCallback(
-    (id) => {
-      todoList.setComplete(id);
-      console.log('handleCompleteTodo сработала');
-    },
-    [todoList]
-  );
-
-  const handleEditTodo = useCallback(
-    (id) => {
-      todoList.setEdit(id);
-      console.log('handleEditTodo сработала');
-    },
-    [todoList]
-  );
-
-  const handleAgreeEdit = useCallback(
-    (title, id) => {
-      todoList.setAgreeEdit(title, id);
-      console.log('handleAgreeEdit сработала');
-    },
-    [list]
-  );
-
-  const handleUndoEdit = useCallback(
-    (id) => {
-      todoList.setEdit(id);
-      console.log('handleUndoEdit сработала');
-    },
-    [list]
-  );
-
-  console.log('list =>', list);
-  console.log('App render');
   return (
     <div className={s.wrapper}>
       <h1>To do list</h1>
-      <TodoForm onAddTodo={handleAddTodo} />
+      <TodoForm onAddTodo={todoList.addTodo} />
       <Elements
-        onCompleteTodo={handleCompleteTodo}
-        onRemoveTodo={handleRemoveTodo}
+        onCompleteTodo={todoList.setComplete}
+        onRemoveTodo={todoList.removeTodo}
         List={todoList.todoItems}
-        onEditTodo={handleEditTodo}
-        onAgreeEdit={handleAgreeEdit}
-        onUndoEdit={handleUndoEdit}
+        onEditTodo={todoList.setEdit}
+        onAgreeEdit={todoList.setAgreeEdit}
+        onUndoEdit={todoList.setEdit}
       />
     </div>
   );
